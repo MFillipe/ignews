05d64b22286439b842af8d80ecd340cd5c86bc25
Matheus Filipe <matheus_filipe00@outlook.com>
2021-03-29 21:57:03 -0300

    feat: add Stripe configuration

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    env: {
+        customKey:
+        "sk_test_51IaVXAEGo7fLFOWtqaFu1pGgKU4jPIbgyT2qRraH9HUau5qL7joTPubT3axxnGzWrvirSu9ScTFuWsdYcmTrJkXL00xKsj8mS7",
+    },
+};
