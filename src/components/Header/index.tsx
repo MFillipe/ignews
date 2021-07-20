e57ef40c558defa464bc9309668de8381061f148
Matheus Filipe <matheus_filipe00@outlook.com>
2021-07-19 23:21:21 -0300

    feat: add ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,26 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
+import React from 'react';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+  
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>  
                 <nav>
-                    <Link href="/" >
-                      <a className={styles.active}>Home</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active} href="/" >
+                      <a>Home</a>
+                    </ActiveLink>
                     
-                    <Link href="/posts"> 
-                      <a>Posts</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active}href="/posts"> 
+                      <a >Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
