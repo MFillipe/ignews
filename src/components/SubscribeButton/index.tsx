3688e3f6d0c59c10a57e19feb1483fee230ff8dc
Matheus Filipe <matheus_filipe00@outlook.com>
2021-07-31 22:17:35 -0300

    feat: add activeSubscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,42 +1,50 @@
 import { useSession, signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
-    priceId: string;
+  priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    const [session] = useSession();
+  const [session] = useSession();
+  const router = useRouter();
 
-    async function handleSubscribe() {
-        if(!session) {
-            signIn('github')
-            return;
-        }
-
-       try {
-           const respose = await api.post('/subscribe');
-
-           const { sessionId } = respose.data;
-
-           const stripe = await getStripeJs();
-
-           await stripe.redirectToCheckout({ sessionId });
-
-       } catch(err) {
-           alert(err.message);
-       }
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return;
     }
 
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-            onClick={handleSubscribe}    
-            >
-                Subscribe now
-        </button>
-    );
+    if(session.activeSubscription) {
+      router.push('/posts');
+      return; 
+    }
+
+
+    try {
+      const respose = await api.post('/subscribe');
+
+      const { sessionId } = respose.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+
+    } catch(err) {
+        alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}    
+    >
+       Subscribe now
+    </button>
+  );
 }
